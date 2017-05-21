78072b5c2c77e5b1dfe343ada4d4da60c687a93d
vagrant <vagrant@localhost.localdomain>
2017-05-21 12:48:25 +0000

    add model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = Hash.new
+    @user[:name] = 'Shohei Aoki'
+    @user[:username] = 'hogehoge'
+    @user[:location] = 'tottori japan'
+    @user[:about] = 'nice to meet you'
   end
 end
